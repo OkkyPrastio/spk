bc0935a41fed302e55184c1facf3ebd09d599f97
OkkyPrastio <okkyprastio0210@gmail.com>
2025-01-27 15:55:36 +0700

    initial commit

---
+++ trial.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T R I A L</title>
+    <style>
+        * {
+            background-color: black;
+            padding: 12px 0px 0px 18px;
+            color: white;
+            font: 20px/1.5 Arial, sans-serif;
+            /* Gaya, ketebalan, ukuran/font-size, tinggi garis, dan jenis font */
+            letter-spacing: 2px;
+            /* Jarak antar huruf */
+            word-spacing: 4px;
+            /* Jarak antar kata */
+        }
+    </style>
+</head>
+
+<body>
+</body>
+
+</html>
+
+<?php
+
+function salam()
+{
+    echo "hai semua";
+}
+
+salam();
